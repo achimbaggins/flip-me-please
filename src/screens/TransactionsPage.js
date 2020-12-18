c8e4d67e60a13cd749dce092881fd2e0261ed555
Achim <achim.networks@gmail.com>
2020-12-19 00:26:13 +0700

    try call api and success

--- src/screens/TransactionsPage.js
+++ src/screens/TransactionsPage.js
@@ -1,8 +1,22 @@
-import React, {useState, useCallback} from 'react'
+import React, {useState, useCallback, useEffect} from 'react'
 import {View, Text, StatusBar,} from 'react-native'
+import { useSelector, useDispatch } from 'react-redux'
+
 import {navigate} from '../helpers/navigasi'
+import {clear_data, set_transactions_list, get_data_transaksi} from '../redux/actions/transactionsActions'
 
 export default function TransactionsPage(props){
+    const dispatch = useDispatch()
+    const {transactionsReducer} = useSelector(state => state)
+
+    useEffect(()=>{
+        console.log(transactionsReducer,'======')
+        dispatch(get_data_transaksi())
+        .then(res =>{
+            console.log('res====', res)
+        })
+    },[])
+
     return(
         <View style={{flex: 1, justifyContent:'center', alignItems:'center'}}>
             <Text onPress={()=>navigate('TransactionDetail')}>Menuju Transaction Detail</Text>
